72092c1e3e87789082c8de883acd3b7ceeacd6ae
Prince Okpoziakpo <princeokpoziakpo@gmail.com>
2022-09-21 06:17:37 -0400

    Made changes to tetris

--- client/tetris/script.js
+++ client/tetris/script.js
@@ -1,4 +1,50 @@
 // Fires when the initial HTML document has been completely loaded, and parsed
 document.addEventListener('DOMContentLoaded', () => {
+  const grid = document.querySelector('.grid');
+  let squares = Array.from(document.querySelectorAll('.grid div'));
+  const ScoreDisplay = document.querySelector('#score');
+  const StartButton = document.querySelector('#start-button');
+  const width = 10;
 
+  // The Tetrominoes
+  const lTetromino = [
+    [1, width + 1, width * 2 + 1, 2],
+    [width, width + 1, width + 2, width * 2 + 2],
+    [1, width + 1, width * 2 + 1, width * 2],
+    [width, width * 2, width * 2 + 2]
+  ];
+
+  const zTetromino = [
+    [0, width, width + 1, width * 2 + 1],
+    [width + 1, width + 2, width * 2, width * 2 + 1],
+    [0, width, width + 1, width * 2 + 1],
+    [width + 1, width + 2, width * 2, width * 2 + 1]
+  ];
+
+  const tTetromino = [
+    [1, width, width + 1, width + 2],
+    [1, width + 1, width + 2, width * 2 + 1],
+    [width, width + 1, width + 2, width * 2 + 1],
+    [1, width, width + 1, width * 2 + 1]
+  ];
+
+  const oTetromino = [
+    [0, 1, width, width + 1],
+    [0, 1, width, width + 1],
+    [0, 1, width, width + 1],
+    [0, 1, width, width + 1]
+  ];
+
+  const iTetromino = [
+    [1, width + 1, width * 2 + 1, width * 3 + 1],
+    [width, width + 1, width + 2, width + 3],
+    [1, width + 1, width * 2 + 1, width * 3 + 1],
+    [width, width + 1, width + 2, width + 3]
+  ];
+
+  const allTetrominoes = [lTetromino, zTetromino, tTetromino, oTetromino, iTetromino];
+  let currentPosition = 4;
+  let current = allTetrominoes[0][0];
+
+  console.log(allTetrominoes);
 });
