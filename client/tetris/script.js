d9174c7eb10dd16c7556bcb02945200c09f6c9fb
Prince Okpoziakpo <princeokpoziakpo@gmail.com>
2022-09-26 08:46:30 -0400

    Added draw and undraw functions to the script.js

--- client/tetris/script.js
+++ client/tetris/script.js
@@ -1,7 +1,7 @@
 // Fires when the initial HTML document has been completely loaded, and parsed
 document.addEventListener('DOMContentLoaded', () => {
   const grid = document.querySelector('.grid');
-  let squares = Array.from(document.querySelectorAll('.grid div'));
+  const squares = Array.from(document.querySelectorAll('.grid div'));
   const ScoreDisplay = document.querySelector('#score');
   const StartButton = document.querySelector('#start-button');
   const width = 10;
@@ -43,8 +43,25 @@ document.addEventListener('DOMContentLoaded', () => {
   ];
 
   const allTetrominoes = [lTetromino, zTetromino, tTetromino, oTetromino, iTetromino];
-  let currentPosition = 4;
-  let current = allTetrominoes[0][0];
+  const currentPosition = 4;
+  const currentRotation = 0;
 
+  // Randomly select a tetromino and its first rotation
+  const random = Math.floor(Math.random() * allTetrominoes.length);
+  const current = allTetrominoes[0][0];
+
+  // draw the Tetromino
+  function draw() {
+    current.forEach(index => {
+      squares[currentPosistion + index].classList.add('tetromino');
+    });
+  }
+
+  // undraw the Tetromino
+  function undraw() {
+    current.forEach(index => {
+      square[currentPosition + index].classList.remove('tetromino');
+    });
+  }
   console.log(allTetrominoes);
 });
