1b71f3b08955e3b759d38e894ded346429f59cce
Prince Okpoziakpo <princeokpoziakpo@gmail.com>
2022-10-11 20:16:33 -0400

    Made changes to lab_5 script

--- server/routes/foodServiceRoutes.js
+++ server/routes/foodServiceRoutes.js
@@ -50,7 +50,7 @@ router
     */
     try { // Try: This code might work! It might not, though. It's important to res - respond, to seal the request - regardless.
       console.log('You touched the foodService Route!');
-      console.log('req.foodServiceData results in foodServicePG GET', req.foodServiceData.length); // this information comes in through the middleware above
+      console.log('req.foodServiceData results in foodServicePjG GET', req.foodServiceData.length); // this information comes in through the middleware above
 
       let reply;
       if (req.query?.resto) {
